7e029bcc7b7edee114067ca7637198d07694b4e8
Deepak Goel <goeldeepak747@gmail.com>
2026-05-08 21:51:13 +0530

    updated project files

--- All questions/MaxSubArray.java
+++ All questions/MaxSubArray.java
@@ -1,17 +1,12 @@
-public class MaxSubArray {
-    public static void main(String[] args){
-        int[]arr={2,1,5,1,3,2};
-        int k=3;
-        int windowSum=0;
-        for(int i=0;i<k;i++){
-            windowSum+=arr[i];
+import java.util.*;
+class maxSubArray {
+    public int maxSubArray(int[] nums) {
+        int curr = 0;
+        int maxsum = nums[0];
+        for(int x : nums){
+            curr = Math.max(x,curr+x);
+            maxsum = Math.max(maxsum,curr);
         }
-        int maxSum=windowSum;
-        for(int right=k;right<arr.length;right++){
-            windowSum+=arr[right];
-            windowSum-=arr[right-k];
-            maxSum=Math.max(maxSum,windowSum);
-        }
-        System.out.println("maximum Sum ="+maxSum);
+        return maxsum;
     }
 }
