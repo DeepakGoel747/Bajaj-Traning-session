cf952f51ac842c2c392052fe6c4a26e54255af9a
Deepak Goel <goeldeepak747@gmail.com>
2026-05-08 21:44:36 +0530

    updated training session codes

---
+++ All questions/DeleteDuplicates.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp=head;
+
+        if(head==null) return null;
+
+        while(temp!=null){
+            if(temp.next!=null && temp.next.val==temp.val){
+                temp.next=temp.next.next;
+            }else{
+                temp=temp.next;
+            }
+        }
+
+        return head;
+    }
+}
